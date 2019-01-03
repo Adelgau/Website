6e1b743ea35b60c4c4962e5ffb317ffca3c21f89
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-03 13:42:33 +0100

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,16 @@
+//popup window
+
+setTimeout(function(){ 
+var answer = confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+if (answer) {
+  alert(location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+    
+}
+else {
+    //some code
+} }, 1000);
+
+
+
+
